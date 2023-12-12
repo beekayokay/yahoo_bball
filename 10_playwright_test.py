3c4708a2d2e4e641ff45e982ff76a8376ca2d526
beekayokay <kobrian6@gmail.com>
2023-12-12 13:16:25 -0700

    added 30 and 14 days

--- 10_playwright_test.py
+++ 10_playwright_test.py
@@ -15,8 +15,21 @@ with sync_playwright() as p:
     page.locator('#ContentPlaceHolder1_DDDURATION').select_option('30')
     page.locator('#ContentPlaceHolder1_CBTO').uncheck()
     page.wait_for_timeout(10000)
-    page.screenshot(path=f'{os.getenv('FILES_DIR')}/test.png', full_page=True)
     content = page.content()
-    with open(f'{os.getenv('FILES_DIR')}/content.html', 'w+', encoding='utf-8') as f:
+    with open(f'{os.getenv('FILES_DIR')}/content_30.html', 'w+', encoding='utf-8') as f:
         f.write(content)
     browser.close()
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False, slow_mo=50)
+    page = browser.new_page()
+    page.goto(URL)
+    page.locator('#ContentPlaceHolder1_DDSHOW').select_option('900')
+    page.locator('#ContentPlaceHolder1_DropDownList1').select_option('On')
+    page.locator('#ContentPlaceHolder1_DDDURATION').select_option('14')
+    page.locator('#ContentPlaceHolder1_CBTO').uncheck()
+    page.wait_for_timeout(10000)
+    content = page.content()
+    with open(f'{os.getenv('FILES_DIR')}/content_14.html', 'w+', encoding='utf-8') as f:
+        f.write(content)
+    browser.close()   
